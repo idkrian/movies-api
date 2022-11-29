d03a642987e9295c798dfcb80c86d0b47b61e979
Rian <riansouza0@gmail.com>
2022-11-29 02:28:13 -0300

    feat(Movie): get one movie request

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -8,11 +8,32 @@ namespace MovieAPI.Controllers
     public class MovieController : ControllerBase
     {
         private static List<Movie> movies = new List<Movie>();
+        private static int id = 1;
 
         [HttpPost]
-        public void AddMovie([FromBody] Movie movie)
+        public IActionResult AddMovie([FromBody] Movie movie)
         {
+            movie.Id = id++;
             movies.Add(movie);
+            return CreatedAtAction(nameof(GetOneMovie), new { Id = movie.Id }, movie);
+        }
+
+        [HttpGet]
+        public IActionResult GetMovie()
+        {
+            return Ok(movies);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetOneMovie(int id)
+        {
+            Movie movie = movies.FirstOrDefault(filme => filme.Id == id);
+            if (movie != null)
+            {
+                return Ok(movie);
+            }
+            return NotFound();
+
         }
     }
 }
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -7,15 +7,15 @@ namespace MovieAPI.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "O nome é obrigatório")]
-        public string? Name { get; set; }
+        public string Name { get; set; }
 
         [Required(ErrorMessage = "O diretor é obrigatório")]
-        public string? Director { get; set; }
+        public string Director { get; set; }
 
         [Range(20, 300, ErrorMessage = "Digite um valor válido")]
         public int Duration { get; set; }
 
-        public string? Genre { get; set; }
+        public string Genre { get; set; }
 
     }
 
